f1de14c6e1703b9b17cac02045b4ad70c3ad5739
Diego Jimenez <djimeneztamame@gmail.com>
2023-11-18 14:31:00 +0100

    New Calculator 1 Instance of each Method (Add, Subtract, Multiply, Divide) Output results

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        double result1 = calculator.add(1,2);
+        double result2 = calculator.subtract(2,5);
+        double result3 = calculator.multiply(3,2);
+        double result4 = calculator.divide(6,2);
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
     }
 }
