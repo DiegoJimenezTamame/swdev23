21b1bf5b7f245d8da0ac411c793c0e832e48ea01
Diego Jimenez <djimeneztamame@gmail.com>
2023-11-18 15:12:16 +0100

    1 Instance of squareRoot 1 Instance of exp2
    Output results

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,15 @@ public class Main {
         double result2 = calculator.subtract(2,5);
         double result3 = calculator.multiply(3,2);
         double result4 = calculator.divide(6,2);
+        double result5 = calculator.squareRoot(49);
+        double result6 = calculator.exp2(5);
 
         System.out.println(result1);
         System.out.println(result2);
         System.out.println(result3);
         System.out.println(result4);
+        System.out.println(result5);
+        System.out.println(result6);
 
     }
 }
