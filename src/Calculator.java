b28b9820be1cf9c9ec5bcade6759d9bfaa209014
Diego Jimenez <djimeneztamame@gmail.com>
2023-11-18 14:55:38 +0100

    Implement Square Root Method

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,4 +15,8 @@ public class Calculator {
     public double divide(double number1, double number2) {
         return number1 / number2;
     }
+
+    public double squareRoot(double number){
+        return Math.sqrt(number);
+    }
 }
